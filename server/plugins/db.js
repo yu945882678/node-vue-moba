6df9b211243f8d08076a916a7373e79f841b4fa6
zhuangyang <945882678@qq.com>
2020-10-25 22:57:21 +0800

    重新开始

--- server/plugins/db.js
+++
@@ -1,7 +0,0 @@
-module.exports = app=>{
-    const mongoose = require('mongoose')
-    mongoose.connect('mongodb://localhost:27017/node-vue-moba',{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-}
