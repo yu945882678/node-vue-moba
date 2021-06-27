faab2a030da28eac61a091363a5018e81e68bd9d
yumu <945882678@qq.com>
2021-06-27 22:45:18 +0800

    分类增删改查

---
+++ server/plugins/db.js
@@ -0,0 +1,10 @@
+module.exports = app => {
+    const mongoose = require('mongoose')
+    mongoose.connect('mongodb://127.0.0.1/node-vue-moba', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+
+
+}
